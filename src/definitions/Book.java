369db0493bdd0e01b5f249a424251b54674ef8c5
Lakshya Yadav <lakmtj@gmail.com>
2020-08-29 19:30:39 +0530

    final commit Library.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,10 +1,77 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:49 PM
+ *  User: Lakshya Yadav
+ *  Date: 29/08/20
+ *  Time: 6:51 PM
  *  File Name : Book.java
  * */
 package definitions;
 
+import java.util.Objects;
+
 public class Book {
+    private String nameOfBook;
+    private String nameOfAuthorOfTheBook;
+    private String ISBNNumberOfTheBook;
+
+    public Book() {
+        this.nameOfBook = "A Song of Ice and Fire";
+        this.nameOfAuthorOfTheBook = "George R.R. Martin";
+        this.ISBNNumberOfTheBook = "1982736452938";
+    }
+
+    public Book(String nameOfBook, String nameOfAuthorOfTheBook, String ISBNNumberOfTheBook) {
+        this.nameOfBook = nameOfBook;
+        this.nameOfAuthorOfTheBook = nameOfAuthorOfTheBook;
+        this.ISBNNumberOfTheBook = ISBNNumberOfTheBook;
+    }
+
+    public String getBookName() {
+        return nameOfBook;
+    }
+
+    public void setBookName(String nameOfBook) {
+        this.nameOfBook = nameOfBook;
+    }
+
+    public String getAuthorName() {
+        return nameOfAuthorOfTheBook;
+    }
+
+    public void setAuthorName(String nameOfAuthorOfTheBook) {
+        this.nameOfAuthorOfTheBook = nameOfAuthorOfTheBook;
+    }
+
+    public String getISBNNumber() {
+        return ISBNNumberOfTheBook;
+    }
+
+    public void setISBNNumber(String ISBNNumberOfTheBook) {
+        ISBNNumberOfTheBook = ISBNNumberOfTheBook;
+    }
+
+    @Override
+    public String toString() {
+        return "Book Name: " + getBookName() + ", " + "Author Name: " + getAuthorName() + ", " + "ISBN Number: "
+                + getISBNNumber() + ".";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Book book = (Book) o;
+        return getAuthorName() == book.getAuthorName() && getISBNNumber() == book.getISBNNumber()
+                && Objects.equals(getBookName(), book.getBookName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBookName(), getAuthorName(), getISBNNumber());
+    }
+
 }
+
--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,10 +1,55 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:50 PM
+ *  User: Lakshya yadav
+ *  Date: 29/08/20
+ *  Time: 6:51 PM
  *  File Name : Library.java
  * */
 package definitions;
 
+import java.util.Arrays;
+
 public class Library {
+    private Book[] booksCurrentlyAvailable;
+
+    public Library() {
+        this.booksCurrentlyAvailable = new Book[10];
+        for (int i = 0; i < booksCurrentlyAvailable.length; i++) {
+            booksCurrentlyAvailable[i] = new Book();
+        }
+    }
+
+    public Library(Book[] booksCurrentlyAvailable) {
+        this.booksCurrentlyAvailable = booksCurrentlyAvailable;
+    }
+
+    public Book[] getBooksAvailable() {
+        return booksCurrentlyAvailable;
+    }
+
+    public void setBooksAvailable(Book[] booksAvailable) {
+        this.booksCurrentlyAvailable = booksAvailable;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(booksCurrentlyAvailable);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Library library = new Library();
+        return Arrays.equals(getBooksAvailable(), library.getBooksAvailable());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getBooksAvailable());
+    }
+
 }
